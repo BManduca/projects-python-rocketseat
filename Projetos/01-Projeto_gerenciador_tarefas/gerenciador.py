799bb8ba12c911c1eb003e54ec7e154da4ed0c17
BManduca <brunnomanducarfe@gmail.com>
2024-06-10 20:46:25 -0300

    feat: finish project task manager

--- Projetos/01-Projeto_gerenciador_tarefas/gerenciador.py
+++ Projetos/01-Projeto_gerenciador_tarefas/gerenciador.py
@@ -32,14 +32,9 @@ def printTextCentralized(msg, wid, cor=0):
     print(message)
     print(colors[0], end='')
 
-def imprimirLinha20(cor=0):
+def imprimirLinha(cor=0, wid=0):
     print(colors[cor], end='')
-    print('-=-'*20)
-    print(colors[0], end='')
-
-def imprimirLinha30(cor=0):
-    print(colors[cor], end='')
-    print('-=-'*30)
+    print('-=-'*wid)
     print(colors[0], end='')
 
 def add_task(lista_tarefas, name_task):
@@ -51,9 +46,9 @@ def add_task(lista_tarefas, name_task):
     tarefa = {'tarefa': name_task, 'completada': False}
     lista_tarefas.append(tarefa)
     jumpLine()
-    imprimirLinha30(2)
+    imprimirLinha(2,30)
     printTextCentralized(f'Tarefa {name_task} foi adicionada com sucesso!', 80, 2)
-    imprimirLinha30(2)
+    imprimirLinha(2,30)
     jumpLine()
     time.sleep(4)
     system('clear')
@@ -61,8 +56,8 @@ def add_task(lista_tarefas, name_task):
 
 def view_tasks(lista_tarefas):
     jumpLine()
-    imprimirLinha20(4)
-    printTextCentralized('LISTA DE TAREFAS: ', 60, 4)
+    imprimirLinha(4,26)
+    printTextCentralized('LISTA DE TAREFAS: ', 70, 4)
     for index, tarefa in enumerate(lista_tarefas, start=1):
         """
             status = '✓' if tarefa['completada'] else ' '
@@ -76,17 +71,87 @@ def view_tasks(lista_tarefas):
         """
         status = '✓' if tarefa['completada'] else ' '
         nome_tarefa = tarefa['tarefa']
-        printTextCentralized(f'{index}. [{status}] {nome_tarefa}', 60, 4)
-    imprimirLinha20(4)
-    time.sleep(7)
+        printTextCentralized(f'{index}. [{status}] {nome_tarefa}', 70, 4)
+    imprimirLinha(4,26)
+    jumpLine()
+    time.sleep(4)
     system('clear')
+    return
 
+def view_tasks_v2(lista_tarefas):
+    jumpLine()
+    imprimirLinha(4,26)
+    printTextCentralized('LISTA DE TAREFAS: ', 66, 4)
+    for index, tarefa in enumerate(lista_tarefas, start=1):
+        status = '✓' if tarefa['completada'] else ' '
+        nome_tarefa = tarefa['tarefa']
+        printTextCentralized(f'{index}. [{status}] {nome_tarefa}', 66, 4)
+    imprimirLinha(4,26)
+    jumpLine()
+    return
+
+def update_name_task(lista_tarefas, index_task, new_name_task):
+    """
+        como o index inicia em zero, precisamos passar o
+        valor decrementado, para não ocorrer erro
+    """
+    index_task_adjust = int(index_task) - 1
+    if len(lista_tarefas) > index_task_adjust >= 0:
+        lista_tarefas[index_task_adjust]['tarefa'] = new_name_task
+        jumpLine()
+        imprimirLinha(5,30)
+        printTextCentralized(f'Tarefa {index_task} atualizada para {new_name_task}.', 70, 5)
+        imprimirLinha(5,30)
+        time.sleep(5)
+        system('clear')
+    else:
+        jumpLine()
+        imprimirLinha(1,20)
+        printTextCentralized('ÍNDICE DE TAREFA INVÁLIDO',60,1)
+        printTextCentralized('FAVOR INSERIR UM ÍNDICE DE TAREFA VÁLIDO!',60,1)
+        imprimirLinha(1,20)
+        jumpLine()
+        time.sleep(5)
+        system('clear')
+    return
+
+def complete_task(lista_tarefas, index_task):
+    index_task_to_complete = int(index_task) - 1
+
+    if len(lista_tarefas) > index_task_to_complete >= 0:
+        lista_tarefas[index_task_to_complete]['completada'] = True
+        jumpLine()
+        imprimirLinha(2,20)
+        printTextCentralized(f'TAREFA {index_task} FOI MARCADA COMO COMPLETADA',60,2)
+        imprimirLinha(2,20)
+        jumpLine()
+        time.sleep(5)
+        system('clear')
+    else:
+        jumpLine()
+        imprimirLinha(1,20)
+        printTextCentralized('ÍNDICE DE TAREFA INVÁLIDO',60,1)
+        printTextCentralized('FAVOR INSERIR UM ÍNDICE DE TAREFA VÁLIDO!',60,1)
+        imprimirLinha(1,20)
+        jumpLine()
+        time.sleep(5)
+        system('clear')
+    return
+
+def delete_complete_tasks(lista_tarefas):
+    imprimirLinha(3,26)
+    for tarefa in lista_tarefas:
+        if tarefa['completada']:
+            tarefas.remove(tarefa)
+    printTextCentralized('TAREFAS COMPLETADAS FORAM DELETADAS COM SUCESSO.',70,3)
+    imprimirLinha(3,26)
+    return
 
 tarefas = []
 
 def showMenu():
     jumpLine()
-    imprimirLinha20(4)
+    imprimirLinha(4,20)
     printTextCentralized('MENU DO GERENCIADOR DE LISTA DE TAREFAS', 60, 4)
     jumpLine()
     printTextCentralized('1. ADICIONAR TAREFA', 60, 4)
@@ -95,7 +160,7 @@ def showMenu():
     printTextCentralized('4. COMPLETAR TAREFA', 60, 4)
     printTextCentralized('5. DELETAR TAREFAS COMPLETADAS', 60, 4)
     printTextCentralized('6. SAIR', 60, 4)
-    imprimirLinha20(4)
+    imprimirLinha(4,20)
 
 
 def main():
@@ -113,21 +178,33 @@ def main():
             add_task(tarefas, nome_tarefa)
         elif choice == '2':
             view_tasks(tarefas)
+        elif choice == '3':
+            view_tasks_v2(tarefas)
+            index_task = input('INSIRA O NÚMERO DA TAREFA QUE SERÁ ATUALIZADA: ')
+            new_name_task = input('INSIRA O NOVO NOME DA TAREFA: ')
+            update_name_task(tarefas, index_task, new_name_task)
+        elif choice == '4':
+            view_tasks_v2(tarefas)
+            index_task = input('INSIRA O NÚMERO DA TAREFA QUE DESEJA COMPLETAR: ')
+            complete_task(tarefas, index_task)
+        elif choice == '5':
+            delete_complete_tasks(tarefas)
+            view_tasks(tarefas)
         elif choice == '6':
             jumpLine()
-            imprimirLinha20(3)
+            imprimirLinha(3,20)
             printTextCentralized('FINALIZANDO PROGRAMA...', 60, 3)
-            imprimirLinha20(3)
+            imprimirLinha(3,20)
             jumpLine()
             time.sleep(2)
             system('clear')
             break
         else:
             jumpLine()
-            imprimirLinha20(1)
+            imprimirLinha(1,20)
             printTextCentralized('OPÇÃO INVÁLIDA!',60,1)
             printTextCentralized('POR FAVOR INSIRA NOVAMENTE UMA OPÇÀO DO MENU!', 60, 1)
-            imprimirLinha20(1)
+            imprimirLinha(1,20)
             time.sleep(5)
             system('clear')
 

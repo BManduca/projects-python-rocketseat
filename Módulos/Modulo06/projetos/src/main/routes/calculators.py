ea46fbbef5e22d42f4406f2509a916f7328ae637
BManduca <brunnomanducarfe@gmail.com>
2024-07-26 17:50:13 -0300

    Finish first Calculator and apply tests with Postman

--- Módulos/Modulo06/projetos/src/main/routes/calculators.py
+++ Módulos/Modulo06/projetos/src/main/routes/calculators.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify, request
+from src.calculators.calculator01 import Calculator1
 
 calc_route_bp = Blueprint('calc_routes', __name__)
 
 @calc_route_bp.route('/calculator/1', methods=['POST'])
 def calculator_01():
-    print(request.json)
+    calc = Calculator1()
+    response = calc.calculate(request)
 
-    return jsonify({'success': True})
+    return jsonify(response)

b2353c1fc9076aa7e9956057a3e385468b0cbbe6
Brunno Manduca <brunnomanducarfe@gmail.com>
2024-09-26 16:08:21 -0300

    config: Add config to connection to db

--- Projetos/04-Projeto_find_a_friend/src/models/sqlite/settings/connection.py
+++ Projetos/04-Projeto_find_a_friend/src/models/sqlite/settings/connection.py
@@ -7,6 +7,7 @@ class DBConnectionhandler:
         self.__engine = None
 
     def connect_to_db(self):
+        # criando uma string de conexão apartir da connection_string
         self.__engine = create_engine(self.__connection_string)
 
     def get_engine(self):

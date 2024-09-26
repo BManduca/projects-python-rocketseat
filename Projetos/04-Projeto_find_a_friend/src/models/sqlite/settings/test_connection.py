2e78f0cadbd5f1da177a810625ce3138a082969c
Brunno Manduca <brunnomanducarfe@gmail.com>
2024-09-26 16:05:49 -0300

    test: Add test for connection to db

--- Projetos/04-Projeto_find_a_friend/src/models/sqlite/settings/test_connection.py
+++ Projetos/04-Projeto_find_a_friend/src/models/sqlite/settings/test_connection.py
@@ -2,12 +2,16 @@ import pytest
 from sqlalchemy.engine import Engine
 from .connection import db_connection_handler
 
+# intuito de conhecimento(educativo)
 @pytest.mark.skip(reason="interação com o banco de dados.")
 # test integration with DB
 def test_connect_to_db():
+    # verificando engine é None
     assert db_connection_handler.get_engine() is None
 
+    # estabelencendo conexao com o DB
     db_connection_handler.connect_to_db()
+    #  atualizando e armazenando o valor da engine
     db_engine = db_connection_handler.get_engine()
 
     assert db_engine is not None

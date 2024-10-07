9b7ec67ce0cde4eedc7d75939abd5b5658f9ef38
Brunno Manduca <brunnomanducarfe@gmail.com>
2024-10-07 18:14:21 -0300

    delete: remove files project

--- Projetos/04-Projeto_find_a_friend/src/controllers/test_pet_deleter_controller.py
+++
@@ -1,9 +0,0 @@
-from src.controllers.pet_deleter_controller import PetDeleterController
-
-def test_delete_pet(mocker):
-    # mock repository generic -> vai receber todas as infos, mas nao vai retornar nada
-    mock_repository = mocker.Mock()
-    controller = PetDeleterController(mock_repository)
-    controller.delete('amiguinho')
-
-    mock_repository.delete_pets.assert_called_once_with('amiguinho')
